7cd8d1ee4f7c548bcad00579226c5ea5e2643460
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2023-02-05 19:32:02 +0300

    uganda requests

---
+++ fetch/fetch_uganda_securities.py
@@ -0,0 +1,4 @@
+from libs.web_fetch import Fetch
+
+use = Fetch('https://www.use.or.ug/content/market-snapshot', 'use')
+use.run()
---
+++ libs/web_fetch.py
@@ -0,0 +1,12 @@
+import requests
+
+class Fetch:
+    def __init__(self, url: str, file_name: str)->None:
+        self.url = url
+        self.file_name = file_name
+
+    def run(self):
+        html_doc = requests.get(self.url).content
+        
+        with open(f'temp/{self.file_name}.html', 'wb') as file:
+            file.write(html_doc)
