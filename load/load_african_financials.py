d42d672ac36866aa5ec8aeded94e1c73b3535ae5
Dalmas.Otieno <tutor.dalmas.otieno@gmail.com>
2023-07-09 03:58:48 +0300

    dotenv

--- load/load_african_financials.py
+++ load/load_african_financials.py
@@ -7,8 +7,8 @@ from libs.constants import securities_data_types, securities_floating_columns
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-dotenv_path = Path('.env')
-load_dotenv(dotenv_path=dotenv_path)
+
+load_dotenv()
 
 class LoadAfricanFinancials:
     def __init__(self, path) -> None:
