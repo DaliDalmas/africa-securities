5e732203ee3ecfa133219e764e9a82add0f44e76
Dalmas.Otieno <tutor.dalmas.otieno@gmail.com>
2023-09-25 00:30:20 +0300

    seleniarm

--- fetch/fetch_jse.py
+++ fetch/fetch_jse.py
@@ -24,7 +24,7 @@ class FetchJSE:
         options = Options()
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--headless")
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.92 Safari/537.36'
         options.add_argument(f'user-agent={user_agent}')
         options.add_experimental_option("detach", True)
         options.add_argument('--no-sandbox')
